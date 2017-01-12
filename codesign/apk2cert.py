df6fdaf05156d19ede109068f03528ea1b7da48a
Dusan Klinec <dusan.klinec@gmail.com>
2017-01-12 21:16:44 +0100

    apk2cert - show also text info

--- codesign/apk2cert.py
+++ codesign/apk2cert.py
@@ -12,10 +12,15 @@ def main():
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Extracts PEM certificates from APK files')
     parser.add_argument('files', nargs=argparse.ZERO_OR_MORE, default=[], help='APK files')
+    parser.add_argument('-t', dest='text', default=False, action='store_const', const=True,
+                        help='show also text representation')
     args = parser.parse_args()
 
     for file_name in args.files:
         apkf = APK(file_name)
+        if args.text:
+            print(apkf.cert_text)
+
         pem = apkf.cert_pem
         print(pem)
 
