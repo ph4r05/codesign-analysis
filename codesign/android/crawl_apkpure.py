b2b020215386246da7217219892def796d77d2b1
Dusan Klinec <dusan.klinec@gmail.com>
2017-01-13 00:01:20 +0100

    crawler fixes

--- codesign/android/crawl_apkpure.py
+++ codesign/android/crawl_apkpure.py
@@ -18,6 +18,15 @@ import math
 from lxml import html
 from collections import OrderedDict
 
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+try:
+    from codesign import utils
+except:
+    import utils
+
 
 logger = logging.getLogger(__name__)
 coloredlogs.install(level=logging.INFO)
