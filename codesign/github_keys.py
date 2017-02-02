7d4adff329ad690507457904c7f40ef0755a2865
Dusan Klinec <dusan.klinec@gmail.com>
2017-02-02 10:11:17 +0100

    github since id refreshing in the status file

--- codesign/github_keys.py
+++ codesign/github_keys.py
@@ -460,6 +460,9 @@ class GitHubLoader(Cmd):
         new_job = DownloadJob(url=users_url, jtype=DownloadJob.TYPE_USERS)
         self.link_queue.put(new_job)
 
+        if self.since_id < max_id:
+            self.since_id = max_id
+
         logger.info('[%02d, usr=%s, remaining=%s] Processed users link %s, Next since: %s. New users: [%s]'
                     % (self.local_data.idx, self.local_data.last_usr, self.local_data.last_remaining,
                        len(github_users)+1, max_id, ', '.join([str(x.user_name) for x in github_users])))
