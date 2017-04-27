1680b6a5e4131e798e9d7176b66ce3bf25308899
Dusan Klinec <dusan.klinec@gmail.com>
2017-04-28 00:09:58 +0200

    eco job generation update

--- assets/jobs-eco/gen.py
+++ assets/jobs-eco/gen.py
@@ -38,8 +38,9 @@ for i in range(total_proc):
 with open('enqueue.sh', 'w') as fh:
     fh.write('#!/bin/bash\n\n')
     for i in range(total_proc):
-        fh.write('qsub -l select=1:ncpus=1:mem=24gb:scratch_local=1gb:brno=True -l walltime=24:00:00 '
-                 './sonar-eco-process-%02d.sh \n' % i)
+        ram = 60 if i == 0 else 24
+        fh.write('qsub -l select=1:ncpus=1:mem=%sgb:scratch_local=1gb:brno=True -l walltime=24:00:00 '
+                 './sonar-eco-process-%02d.sh \n' % (ram, i))
 
 os.system('chmod +x *.sh')
 
