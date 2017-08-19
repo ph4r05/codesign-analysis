7766c2608c8c2082e20520875402c9e1fc43012b
Dusan Klinec <dusan.klinec@gmail.com>
2017-08-19 15:47:55 +0200

    ee - code generation changed, take also y2ks

--- codesign/ee2.py
+++ codesign/ee2.py
@@ -224,18 +224,16 @@ def random_isikukood(serial_dist=None):
     Generates random personal code / isikukood
     :return:
     """
-    century = 0  # random.randint(0, 1)
-    sex = random.randint(3, 4)
+    # pick those likely alive and already having IDs
+    year = random.randint(1950, 2002)
+    century = (year - 1800) / 100
 
-    d1 = sex + century
-
-    # year - century 0 -> live people.. start with 50
-    #        century 1 -> adult people, none :P
-    year = random.randint(50, 99)
+    sex = random.randint(1, 2)  # 1,2 for 18xx | 3,4 for 19xx | 5,6 for 20xx
+    d1 = sex + 2*century
 
     # generate random day & month in that year - ordinals
-    minord = datetime.date(year=1900+year, month=1, day=1).toordinal()
-    maxord = datetime.date(year=1900+year, month=12, day=31).toordinal()
+    minord = datetime.date(year=year, month=1, day=1).toordinal()
+    maxord = datetime.date(year=year, month=12, day=31).toordinal()
     randord = random.randint(minord, maxord)
     rnddate = datetime.date.fromordinal(randord)
 
@@ -244,7 +242,7 @@ def random_isikukood(serial_dist=None):
     if serial_dist is not None:
         serial = serial_dist.rvs()
 
-    code = '%d%02d%02d%02d%03d' % (d1, year, rnddate.month, rnddate.day, serial)
+    code = '%d%02d%02d%02d%03d' % (d1, year % 100, rnddate.month, rnddate.day, serial)
     return code + control_nr(code)
 
 
